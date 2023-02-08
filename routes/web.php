e7b14c25c9d8dd53df1aad8c955336562cdb062f
Daniellizh <dl.lucky.36@gmail.com>
2023-02-08 12:13:14 +0300

    breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,23 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    //Products
-    Route::get('/add-product', [App\Http\Controllers\ProductController::class, 'create'])->name('add-product');
-    Route::post('/add-product', [App\Http\Controllers\ProductController::class, 'store'])->name('products.store');
-    Route::get('/edit-product/{id}', [App\Http\Controllers\ProductController::class, 'edit'])->name('products.edit');
-    Route::put('/products/edit-product/{id}', [App\Http\Controllers\ProductController::class, 'update'])->name('products.update');
-    Route::delete('/add-product/{id}', [App\Http\Controllers\ProductController::class, 'destroy'])->name('products.destroy');
-
-    Route::resource('roles', App\Http\Controllers\RoleController::class);
 });
-
 
 require __DIR__.'/auth.php';
